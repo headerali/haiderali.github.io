ad6c3ec0bc9449d61c2d6a834ed385cd68c52764
Haider Ali <haider.ali@venturedive.com>
2020-05-10 02:01:39 +0500

    Updates

---
+++ component---src-pages-index-js-4b31e5fe012fbe336ee0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{RXBc:function(e,a,t){"use strict";t.r(a);var n=t("q1tI"),c=t.n(n);t("VxdY");a.default=function(){return c.a.createElement("div",{className:"main-fold-bg"},c.a.createElement("div",{className:"title-container"},c.a.createElement("h1",{className:"name"},"COMING SOON"),c.a.createElement("span",{className:"sub-title"},"SITE UNDER CONSTRUCTION")))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-4b31e5fe012fbe336ee0.js.map
